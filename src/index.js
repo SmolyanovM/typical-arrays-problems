18c00c41b5d3f8d5a80e9376a157950b221031d2
SmolyanovM <ratlord85@gmail.com>
2021-03-14 17:00:46 +0600

    fix: fix code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(Array.isArray(array) == false || array.length <= 0) {
+    return 0;
+  } else {
+    return array.reduce(function (p, v) {
+      return ( p < v ? p : v );
+    });
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(Array.isArray(array) == false || array.length <= 0) {
+    return 0;
+  } else {
+    return array.reduce(function (p, v) {
+      return ( p > v ? p : v );
+    });
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(Array.isArray(array) == false || array.length <= 0) {
+    return 0;
+  } else {
+    var total = 0;
+      for(var i = 0; i < array.length; i++) {
+      total += array[i];
+    }
+    var arrAvg = total / array.length;
+    return arrAvg;
+  }
 }
